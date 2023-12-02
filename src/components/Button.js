c92e29a2ad7308241da36d5242a750947a5bc6cb
Tommy Tan <tommyt.develop@gmail.com>
2023-12-02 00:00:00 -0500

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+export const LbButton = ({ className, children, ...rest }) => {
+  return (
+    <button className={"btn btn-sm lb-btn-color border border-gray-800/50 " + className} {...rest}>
+      {children}
+    </button>
+  );
+}
+                         
---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,7 @@
+import { checkSignin } from "../utils/auth";
+
+export const ProtectedRoute = ({ children }) => {
+  checkSignin(window.location.pathname);
+  return children;
+};
+                         
